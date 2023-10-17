0a7d4bbbe363bb72d03d3e1be45ff763bc784361
Diego <Conkergox98@gmail.com>
2023-10-17 14:07:05 +0200

    Interfaz de Sign acabada

--- src/modelo/Sign.java
+++ src/modelo/Sign.java
@@ -1,9 +1,36 @@
 package modelo;
 
+import excepciones.IncorrectCredentialsException;
+import excepciones.ConnectException;
+import excepciones.UserNotFoundException;
+import excepciones.UserAlreadyExistsException;
+
 /**
+ * Interfaz principal del programa, tiene los métodos para hacer el registro y
+ * el inicio de sesión con sus respectivas excepciones a lanzar
  *
- * @author 2dam
+ * @author Diego
  */
 public interface Sign {
-    
+
+    /**
+     * Método executeLogin, se encarga de hacer registro a nuestra aplicación
+     *
+     * @param user
+     * @return user
+     * @throws excepciones.ConnectException
+     * @throws excepciones.IncorrectCredentialsException
+     * @throws excepciones.UserAlreadyExistsException
+     */
+    public User excecuteLogin(User user) throws ConnectException, IncorrectCredentialsException, UserAlreadyExistsException;
+
+    /**
+     * Método executeSignIn, se encarga de hacer el inicio de sesión
+     *
+     * @param user
+     * @return user
+     * @throws excepciones.UserNotFoundException
+     * @throws excepciones.ConnectException
+     */
+    public User executeSignIn(User user) throws UserNotFoundException, ConnectException;
 }

4301640c0043b36a71482d7258b238b7102e5ab7
Ander <anderespinofernandez@gmail.com>
2023-10-17 13:57:53 +0200

    Mensaje y Tipo de Mensaje terminados!

--- src/modelo/Message.java
+++ src/modelo/Message.java
@@ -1,12 +1,39 @@
-
 package modelo;
 
 import java.io.Serializable;
 
 /**
+ * Esta clase sirve recoger y mandar los mensajes que se enviaran al servidor.
  *
- * @author 2dam
+ * @author Ander
  */
-public class Message implements Serializable{
-    
+public class Message implements Serializable {
+
+    //Atributos 
+    private MessageType msg;
+    private User user;
+
+    /**
+     * Aqui esta los getters y los setters del msg y de user.
+     *
+     * @return msg (getMessage)
+     * @return user (getUser)
+     *
+     */
+    public MessageType getMsg() {
+        return msg;
+    }
+
+    public void setMsg(MessageType msg) {
+        this.msg = msg;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
 }

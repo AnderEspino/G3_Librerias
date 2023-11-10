4a12ea4f3da8e9caae7497129ca36709ad10bd19
Diego <Conkergox98@gmail.com>
2023-11-10 09:38:45 +0100

    Proyecto Libreria acabado

---
+++ src/excepciones/package-info.java
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package excepciones;
+
+/**
+ * Paquete con las excepciones utilizadas tanto en el servidor como en el
+ * cliente.
+ *
+ * @author Diego, Adrian, Ander
+ */
